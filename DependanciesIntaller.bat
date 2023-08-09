db124512120a683d0f5453ed3ef8f7a4a363c796
Bo Li <lb284542@gmail.com>
2023-08-09 21:36:15 +0800

    modify install bat

--- DependanciesIntaller.bat
+++ DependanciesIntaller.bat
@@ -1,10 +1,12 @@
 @echo
-call bootstrap-vcpkg
-call vcpkg integrate install
-call vcpkg install assimp
-call vcpkg install glad
-call vcpkg install glfw3
-call vcpkg install glm
-call vcpkg install ms-gltf
-call vcpkg install stb
+::git clone https://github.com/microsoft/vcpkg
+cd c:\vcpkg
+::call bootstrap-vcpkg
+::call vcpkg integrate install
+call vcpkg install assimp:x64-windows
+call vcpkg install glad:x64-windows
+call vcpkg install glfw3:x64-windows
+call vcpkg install glm:x64-windows
+call vcpkg install ms-gltf:x64-windows
+call vcpkg install stb:x64-windows
 pause
